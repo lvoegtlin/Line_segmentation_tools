95121ca5eed2db227c91a0234d4049fe2249aaa8
Lars Voegtlin <lars.voegtlin@unifr.ch>
2019-03-11 23:36:23 +0100

    interpolating heatmap

--- src/create_heatmap.py
+++ src/create_heatmap.py
@@ -1,7 +1,10 @@
 import csv
 import os
+
 import numpy as np
+import itertools
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
 
 from natsort import natsorted
 
@@ -21,22 +24,41 @@ def get_avg_score(path):
 
 
 if __name__ == '__main__':
-    penalties = list(range(2000, 13000, 1000))
-    seams_per = list(range(10, 110, 10))
+    penalties_axis = [500] + list(range(1000, 13000, 1000))
+    seams_per_axis = list(range(10, 110, 10))
+    penalties = []
+    seams_per = []
+    points = []
 
+    scores_old = []
     scores = []
 
     # iteratoe throw all folders
     folders = list(os.walk(BASE_PATH))[0][1]
     folders = natsorted(folders)
 
-    current_penalty = 2000
     penalty_score = []
 
-    for folder in folders:
+    column = 0
+    for i, folder in enumerate(folders):
         penalty, seam = get_params(folder)
         score = get_avg_score(os.path.join(BASE_PATH, folder))
-        scores.append(score)
+        scores_old.append(score)
+
+        points.append([seam, penalty])
+
+        if penalty not in penalties:
+            penalties.append(penalty)
+            scores.append(np.asarray(penalty_score))
+            penalty_score = [score]
+            i = 0
+        else:
+            penalty_score.append(score)
+
+        if seam not in seams_per:
+            seams_per.append(seam)
+
+        i += 1
         # if penalty != current_penalty:
         #     scores.append(penalty_score)
         #     penalty_score = []
@@ -46,27 +68,31 @@ if __name__ == '__main__':
 
     penalties = np.asarray(penalties)
     seams_per = np.asarray(seams_per)
+    # scores = np.asarray(scores_old).reshape((13, 10)).transpose()
     scores = np.asarray(scores)
-    scores = scores.reshape((11, 10))
+
+    # interpolation
+    grid_z0 = griddata(points, scores_old, (np.asarray([seams_per]), penalties[:, None]), method='linear')
 
     # feed them to mathplotlibfig,
     fig, ax = plt.subplots()
-    im = ax.imshow(scores, cmap="YlGn")
+    # im = ax.imshow(scores, cmap="YlGn")
+    im = ax.imshow(grid_z0[0:35].T, cmap="YlGn")
 
     # Create colorbar
     cbar = ax.figure.colorbar(im, ax=ax)
-    cbar.ax.set_ylabel('', rotation=-90, va="bottom")
+    cbar.ax.set_ylabel('line IU', rotation=-90, va="bottom")
 
-    ax.set_xticks(np.arange(len(seams_per)))
-    ax.set_yticks(np.arange(len(penalties)))
+    ax.set_yticks(np.arange(len(seams_per)))
+    ax.set_xticks(np.arange(len(penalties[0:35])))
     # ... and label them with the respective list entries
-    ax.set_xticklabels(seams_per)
-    ax.set_yticklabels(penalties)
+    ax.set_yticklabels(seams_per)
+    ax.set_xticklabels(penalties, rotation=60)
 
-    ax.set_ylabel('Penalty ' + r'$\alpha$')
-    ax.set_xlabel('Seam every ' + r'$\beta$' + ' pixel')
+    ax.set_xlabel('Penalty ' + r'$\beta$')
+    ax.set_ylabel('Seam every ' + r'$\alpha$' + ' pixel')
 
-    ax.set_title("Parameter Robustness Evaluation - line IU")
+    ax.set_title("Parameter Robustness Evaluation")
 
     plt.savefig(os.path.join(BASE_PATH, 'grid_search_heatmap.png'))
 
